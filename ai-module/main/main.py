3b8cf4ae46cffc1353d0130af16719d27fe57e5f
unknown <hoangphuc141204@gmail.com>
2025-11-12 21:00:22 +0700

    add document

--- ai-module/main/main.py
+++ ai-module/main/main.py
@@ -16,7 +16,7 @@ if not os.environ.get("GOOGLE_API_KEY"):
 
 # 1. FIXED: Explicitly define the model_provider
 llm = init_chat_model(
-    "gemini-2.5-flash", model_provider="google_genai" 
+    "gemini-2.5-flash", model_provider="google_genai" , temperature = 0
 )
 
 
